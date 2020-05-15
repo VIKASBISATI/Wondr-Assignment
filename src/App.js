b2b1e89544debeac39c6cbbc2c8dfb4e7c3cc4c9
Bisati Sai Venkata Vikas <vikas.bisati@Bisati-sai-MacBook-Air.local>
2020-05-15 11:40:35 +0530

    read me updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Navbar from "./components/navBar";
 import "./App.css";
 import ImageGallery from "./components/imageGallery";
 import ImagesList from "./components/images";
@@ -8,25 +7,16 @@ import ImagesList from "./components/images";
 //Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
-//Mui
-// import themeStyles from "./utils/theme";
-// import { MuiThemeProvider } from "@material-ui/core/styles";
-// import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-
-// const theme = createMuiTheme(themeStyles);
 function App() {
   return (
-    // <MuiThemeProvider theme={theme}>
       <Provider store={store}>
         <Router>
           <Switch>
-            <Route exact path="/" component={ImageGallery} />
-            <Route exact path="/images" component={ImagesList} />
+            {/* <Route exact path="/" component={ImageGallery} /> */}
+            <Route path="/" component={ImagesList} />
           </Switch>
         </Router>
       </Provider>
-    // </MuiThemeProvider>
   );
 }
 
